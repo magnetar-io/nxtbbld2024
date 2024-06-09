32bff41c4c2b8c02dfd02105fdfdbb5d4d260579
gschleusner1972 <gschleusner@gmail.com>
2024-06-09 08:19:44 -0400

    Innitial Commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    if (app) {
+      app.innerHTML = "<h1>Hello, World!</h1>";
+    }
+  });
+  
